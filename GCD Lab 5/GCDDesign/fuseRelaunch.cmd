3f9f8fe4a5a68f0cfc17a94f145282f8b2eb4dab
Angel30Luna <60517430+Angel30Luna@users.noreply.github.com>
2020-04-30 12:28:05 -0400

    All Lab Codes

--- GCD Lab 5/GCDDesign/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/Xilinx/Projects/Lab 5/GCDDesign/GCDTop_testbench_isim_beh.exe" -prj "D:/Xilinx/Projects/Lab 5/GCDDesign/GCDTop_testbench_beh.prj" "work.GCDTop_testbench" "work.glbl" 
